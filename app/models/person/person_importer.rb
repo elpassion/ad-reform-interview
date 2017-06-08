2ec22c22bb03c7b8ae61a98132800a9a8a1d4ea5
Kacper Walanus <kacper@walanus.com>
2017-06-08 11:16:01 +0200

    small improvements in PersonImporter

--- app/models/person/person_importer.rb
+++ app/models/person/person_importer.rb
@@ -3,9 +3,9 @@ require 'csv'
 class Person
   class PersonImporter
     class << self
-      CSV_GENDER_COLUMN = 'sex'
-      CSV_HEIGHT_COLUMN = 'heightIn'
-      CSV_WEIGHT_COLUMN = 'weightLb'
+      CSV_GENDER_COLUMN = 'sex'.freeze; private_constant :CSV_GENDER_COLUMN
+      CSV_HEIGHT_COLUMN = 'heightIn'.freeze; private_constant :CSV_HEIGHT_COLUMN
+      CSV_WEIGHT_COLUMN = 'weightLb'.freeze; private_constant :CSV_WEIGHT_COLUMN
 
       # This is designed to be fast, not safe.
       # Use it to import large data from a safe source.
@@ -21,12 +21,18 @@ class Person
           weight = row.fetch(CSV_WEIGHT_COLUMN)
           height = height.to_f.round
           weight = weight.to_f.round
-          sql_values << "(\"#{gender}\", #{height}, #{weight}, \"#{now}\", \"#{now}\"),"
+          sql_values << "('#{gender}', #{height}, #{weight}, '#{now}', '#{now}'),"  # Using #<< for performance
         end
 
-        # Bulk insert (no validation and possible sql injection!)
         sql_values = sql_values.chomp(',')
-        sql = "INSERT INTO #{Person.table_name} (gender, height, weight, created_at, updated_at) VALUES #{sql_values}"
+        sql_columns = 'gender, height, weight, created_at, updated_at'
+
+        sql = <<-SQL
+          INSERT INTO #{Person.table_name} (#{sql_columns}) VALUES #{sql_values}
+        SQL
+
+        # Bulk insert (no validation and possible sql injection!)
+        # Unfortunately, bulk insert is not possible with Arel yet :(
         ActiveRecord::Base.connection.execute(sql)
       end
     end
